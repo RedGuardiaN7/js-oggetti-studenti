f1a76676f5ce3909ff313c006a5afd9bf18f739e
RedGuardiaN7 <artiom.str@hotmail.it>
2022-11-17 13:56:28 +0100

    Milestone 0: creazione dell'array di oggetti

---
+++ script.js
@@ -0,0 +1,37 @@
+/* CONSEGNA:    Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+                Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto. */
+
+//MILESTONE 0: Creazione dell'array contenente tutti i membri del team sotto forma di oggetti
+
+let team_members = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        picture: "img/wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        picture: "img/angela-caroll-chief-editor.jpg",
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        picture: "img/walter-gordon-office-manager.jpg",
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        picture: "img/angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        picture: "img/scott-estrada-developer.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        picture: "img/barbara-ramos-graphic-designer.jpg",
+    }
+]
